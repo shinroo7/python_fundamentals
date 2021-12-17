e07eede52559ac1d0a36f3af311e4d8a97740a29
shinroo7 <shinroo@gmail.com>
2021-12-17 17:13:37 +0000

    variables & data types

---
+++ app.py
@@ -0,0 +1,14 @@
+# Order is important in Python
+print("   /|")
+print("  /  \\")
+print(" /___")
+print(" /____|")
+
+# Variables & Data Type: use underline to connect the variables words (if more than one word). 
+# The data types we often use are String, Number and Boolean.
+character_name = "Freya"
+Character_age = "25"
+
+print("There is a girl called " + character_name + ",")
+print("and she is " + Character_age + " years old")
+
